78b80ec48b773deffc454e1643f6f40d503e1ae1
CharlesAbuga <godfreycharles30@gmail.com>
2024-08-26 15:22:38 +0300

    autocomplete widget

---
+++ lib/Pages/custom_app_bar.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+  static const List<String> _kOptions = <String>[
+    'pizza',
+    'maserati',
+    'chameleon',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Custom AppBar'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: <Widget>[
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your username',
+                ),
+              ),
+              Padding(padding: const EdgeInsets.all(8.0)),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Submit'),
+              ),
+            ]),
+          ),
+        ),
+      );
+    } else if (Platform.isIOS) {
+      return MaterialApp(
+        home: CupertinoPageScaffold(
+          navigationBar: const CupertinoNavigationBar(
+            middle: Text('Custom AppBar'),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+                  Text(
+                    'Type the following words to see if autocomplete works ',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Material(
+                    child: Autocomplete<String>(
+                      fieldViewBuilder: (BuildContext context,
+                          TextEditingController textEditingController,
+                          FocusNode focusNode,
+                          VoidCallback onFieldSubmitted) {
+                        return CupertinoTextField(
+                          controller: textEditingController,
+                          focusNode: focusNode,
+                          onSubmitted: (String value) {
+                            onFieldSubmitted();
+                          },
+                        );
+                      },
+                      optionsBuilder: (TextEditingValue textEditingValue) {
+                        if (textEditingValue.text == '') {
+                          return const Iterable<String>.empty();
+                        }
+                        return _kOptions.where((String option) {
+                          return option
+                              .contains(textEditingValue.text.toLowerCase());
+                        });
+                      },
+                      onSelected: (String selection) {
+                        print('You just selected $selection');
+                      },
+                    ),
+                  ),
+                  Padding(padding: const EdgeInsets.all(8.0)),
+                  CupertinoButton(
+                      child: Text('Submit'),
+                      color: Colors.blue,
+                      onPressed: () {}),
+                ]),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    return Text('Platform not supported');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_edit_controller/Pages/custom_app_bar.dart';
 import 'package:test_edit_controller/Pages/popupmenubutton.dart';
 import 'package:test_edit_controller/cubit/favourite_items_cubit.dart';
 import 'package:test_edit_controller/global_variables.dart';
@@ -46,7 +47,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          home: PopUpButton()),
+          home: CustomAppBar()),
     );
   }
 }
